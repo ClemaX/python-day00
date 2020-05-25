1601a4d52673acbc91c15e5d3a139c85fa949ec0
Clément Hamada <clementhamada@protonmail.com>
2020-05-25 18:09:52 +0200

    ex05

---
+++ ex05/kata00.py
@@ -0,0 +1,4 @@
+t = (19, 42, 21)
+
+print(f"The {len(t)} numbers are: ", end="")
+print(*t, sep=", ")
---
+++ ex05/kata01.py
@@ -0,0 +1,8 @@
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+}
+
+for lang in languages:
+    print(f"{lang} was created by {languages[lang]}")
