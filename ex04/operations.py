01be81094d081e92180b8fed106356eaded5bb1b
Clément Hamada <clementhamada@protonmail.com>
2020-05-25 16:55:21 +0200

    ex04

---
+++ ex04/operations.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def operate(a, b):
+    print(f"Sum:        {a + b}")
+    print(f"Difference: {a - b}")
+    print(f"Product:    {a * b}")
+    if b != 0:
+        print(f"Quotient:   {a / b}")
+        print(f"Remainder:  {a % b}")
+
+
+def print_usage(filename):
+    print(f"Usage: python {filename} <number1> <number2>",
+          "Example:",
+          f"   python {filename} 10 3", sep='\n')
+
+
+if (len(sys.argv) < 3):
+    print("InputError: not enough arguments")
+    print_usage(sys.argv[0])
+elif (len(sys.argv) > 3):
+    print("InputError: too many arguments")
+    print_usage(sys.argv[0])
+else:
+    try:
+        a = int(sys.argv[1])
+        b = int(sys.argv[2])
+        operate(a, b)
+    except ValueError:
+        print("InputError: only numbers")
