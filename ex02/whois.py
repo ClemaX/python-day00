9aaeaa564a8ade6ebc485c009b9f530579bd4b29
Clément Hamada <clementhamada@protonmail.com>
2020-05-25 12:49:46 +0200

    ex02

---
+++ ex02/whois.py
@@ -0,0 +1,10 @@
+import sys
+
+if (len(sys.argv) > 2):
+    print("Error")
+if (len(sys.argv) == 2):
+    try:
+        value = int(sys.argv[1])
+        print("I'm Odd." if value % 2 else "I'm Even.")
+    except ValueError:
+        print("Error")
