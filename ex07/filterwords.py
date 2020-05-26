ad2ed50a7a4573a2c2f02e6a011d81e901f0337b
Clément Hamada <clementhamada@protonmail.com>
2020-05-26 21:59:20 +0200

    ex07: Error if argument not alpha

--- ex07/filterwords.py
+++ ex07/filterwords.py
@@ -13,7 +13,13 @@ except ValueError:
 
 del_punct = sys.argv[1].maketrans('', '', string.punctuation)
 text = sys.argv[1].translate(del_punct)
+
 words = text.split(' ')
+for word in words:
+    if not word.isalpha():
+        print("ERROR")
+        exit(1)
+
 filtered = [word for word in words if len(word) > min_len]
 
 print(filtered)
