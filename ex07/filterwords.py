dfcfebf9934317021fc2d731439ad1dcf4f6243b
Clément Hamada <clementhamada@protonmail.com>
2020-05-26 15:25:08 +0200

    ex07

---
+++ ex07/filterwords.py
@@ -0,0 +1,19 @@
+import sys
+import string
+
+if len(sys.argv) != 3:
+    print("ERROR")
+    exit(1)
+
+try:
+    min_len = int(sys.argv[2])
+except ValueError:
+    print("ERROR")
+    exit(1)
+
+del_punct = sys.argv[1].maketrans('', '', string.punctuation)
+text = sys.argv[1].translate(del_punct)
+words = text.split(' ')
+filtered = [word for word in words if len(word) > min_len]
+
+print(filtered)
